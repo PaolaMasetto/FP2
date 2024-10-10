5605c103fd185c875df6381bab86aa2d623c3bf6
PaolaMasetto <paola.masetto35@my.unitec.edu.mx>
2024-10-09 21:02:30 -0600

    Create PracticaA.cpp

---
+++ PracticaA.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <sstream>
+
+using namespace std;
+
+int main()
+{
+ // Ejercicio 1.1
+	int numeroMaterias;
+	cout << "Introduce el número de materias que estas cursando: ";
+	cin >> numeroMaterias;
+    	cout << "Estas cursando " << numeroMaterias << " materias." << endl;
+
+ // Ejercicio 1.2
+	int numeroCuatrimestre;
+	cout << “Introduce el valor del cuatrimestre que estas cursando: “;
+	cin >> numeroCuatrimestre;
+	cout << “Estas cursando el “ << numeroCuatrimestre << “ cuatrimestre” << endl;
+
+// Ejercicio 1.3
+	string materia;
+	cout << "Introduce el nombre de la materia: ";
+	cin >> materia;
+	cout << "La materia que estas cursando es " << materia << "." << endl;
+
+// Ejercicio 1.4
+	string cuenta;
+	cout << “Introduce tu número de cuenta: “;
+	cin >> cuenta;
+	cout << “Tu numero de cuenta es: “ << cuenta << “.” << endl;
+
+// Ejercicio 1.5
+	string nombre;
+	cout << "Introduce tu nombre: ";
+  cin >> nombre;
+  cout << "Tu nombre es: " << nombre << "." << endl;
+
+// Ejercicio 1.6
+  string apellidoPaterno;
+  cout << "Introduce tu apellido paterno: ";
+  cin >> apellidoPaterno;
+  cout << "Tu apellido paterno es: " << apellidoPaterno << "." << endl;
+
+// Ejercicio 1.7
+  string apellidoMaterno;
+  cout << "Introduce tu apellido materno: ";
+  cin >> apellidoMaterno;
+  cout << "Tu apellido materno es: " << apellidoMaterno << "." << endl;
+
+// Ejercicio 1.8
+  
