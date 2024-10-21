40d67307c2cef2192bbfaaaf74bc5b65cee4e592
PaolaMasetto <paola.masetto35@my.unitec.edu.mx>
2024-10-21 12:38:23 -0600

    Update PracticaB.cpp

--- PracticaB.cpp
+++ PracticaB.cpp
@@ -5,3 +5,14 @@ cout << "(3 + 5 - 1) = " << (3 + 5 - 1) << endl;
 cout << "(8 - 1) = " << (8 - 1) << endl;
 cout << "(1 / 3 * 3) = " (1 / 3 * 3) << endl;
 
+// Ejercicio 1.2
+int num;
+cout << "Ingrese un numero: ";
+cin >> num;
+cout << "La potencia de " << num << " es " << (num * num);
+
+// Ejercicio 1.3
+float yardas;
+cout << "Ingrese las yardas: ";
+cin >> yardas
+
