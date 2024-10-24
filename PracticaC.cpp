5026d85ce8256af07346b8a0a4c09d931c522acb
PaolaMasetto <paola.masetto35@my.unitec.edu.mx>
2024-10-24 08:41:34 -0600

    Create PracticaC.cpp

---
+++ PracticaC.cpp
@@ -0,0 +1,33 @@
+// 1.1
+int num;
+cout << "Ingrese un numero: ";
+cin >> num;
+cout << "El cuadrado de " << num << " es " << (num*num) << endl;
+
+// 1.2
+int nume;
+cout << "Ingrese un numero entero: ";
+cin >> nume;
+cout << nume -- (nume * 2) -- (nume * 3) -- (nume * 4) -- (nume * 5) << endl;
+
+// 1.3
+float peso;
+cout << "Ingrese su peso en kilogramos: ";
+cin >> peso;
+cout << "Usted pesa: " << (peso * 2.2) << " libras." << endl;
+
+// 1.4
+int n1;
+int n2;
+int n3;
+cout << "Ingrese un numero: ";
+cin >> n1;
+cout << "Ingrese un numero: ";
+cin >> n2;
+cout << "Ingrese un numero: ";
+cin >> n3;
+cout << "Total: " << (n1 + n2 + n3);
+cout << "Promedio: " << ((n1 + n2 + n3) / 3) << endl;
+
+// 1.5
+float total;
